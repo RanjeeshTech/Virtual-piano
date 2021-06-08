089b64e716005aa2bab7f01945bfc2239993bed2
RanjeeshTech <ranjeeshr07012002@gmail.com>
2021-06-08 12:38:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,139 @@
+function colorpicker(){
+    let bg = document.getElementById("colors").value;
+    document.getElementById("keyboard").style.backgroundColor =bg;
+    document.getElementById("keyboard").style.boxShadow ='0 10px 25px 0'+bg;
+    document.getElementById("blank1").style.backgroundColor =bg;
+    document.getElementById("blank1").style.boxShadow ='0 10px 25px 0'+bg;
+    document.getElementById("blank2").style.backgroundColor =bg;
+    document.getElementById("blank2").style.boxShadow ='0 10px 25px 0'+bg;
+    document.getElementById("title").style.textShadow ='4px 4px'+bg;
+    document.getElementById("black").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+    document.getElementById("black2").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+    document.getElementById("black3").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+    document.getElementById("black4").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+    document.getElementById("black5").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+    document.getElementById("black6").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+    document.getElementById("black7").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+    document.getElementById("black8").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+    document.getElementById("black9").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+    document.getElementById("black10").style.boxShadow='inset #3a3a3a 0 -10px 0 0,'+bg+' 0 0 0 2px';
+}
+  
+function randomcolor(){
+    let bg = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementById("keyboard").style.backgroundColor ='#'+bg;
+    document.getElementById("keyboard").style.boxShadow ='0 10px 25px 0 #'+bg;
+    document.getElementById("blank1").style.backgroundColor ='#'+bg;
+    document.getElementById("blank1").style.boxShadow ='0 10px 25px 0 #'+bg;
+    document.getElementById("blank2").style.backgroundColor ='#'+bg;
+    document.getElementById("blank2").style.boxShadow ='0 10px 25px 0 #'+bg;
+    document.getElementById("title").style.textShadow ='4px 4px #'+bg;
+    document.getElementById("black").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';
+    document.getElementById("black2").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';
+    document.getElementById("black3").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';
+    document.getElementById("black4").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';
+    document.getElementById("black5").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';
+    document.getElementById("black6").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';
+    document.getElementById("black7").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';
+    document.getElementById("black8").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';
+    document.getElementById("black9").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';
+    document.getElementById("black10").style.boxShadow='inset #3a3a3a 0 -10px 0 0, #'+bg+' 0 0 0 2px';    
+}
+function c3(){
+  var c3_audio=document.getElementById("c3");
+  c3_audio.play();
+}
+function c_3(){
+  var c_3_audio=document.getElementById("c-3");
+  c_3_audio.play();
+}
+function d3(){
+  var d3_audio=document.getElementById("d3");
+  d3_audio.play();
+}
+function d_3(){
+  var d_3_audio=document.getElementById("d-3");
+  d_3_audio.play();
+}
+function e3(){
+  var e3_audio=document.getElementById("e3");
+  e3_audio.play();
+}
+function f3(){
+  var f3_audio=document.getElementById("f3");
+  f3_audio.play();
+}
+function f_3(){
+  var f_3_audio=document.getElementById("f-3");
+  f_3_audio.play();
+}
+function g3(){
+  var g3_audio=document.getElementById("g3");
+  g3_audio.play();
+}
+function g_3(){
+  var g_3_audio=document.getElementById("g-3");
+  g_3_audio.play();
+}
+function a3(){
+  var a3_audio=document.getElementById("a3");
+  a3_audio.play();
+}
+function a_3(){
+  var a_3_audio=document.getElementById("a-3");
+  a_3_audio.play();
+}
+function b3(){
+  var b3_audio=document.getElementById("b3");
+  b3_audio.play();
+}
+
+
+function c4(){
+  var c4_audio =document.getElementById("c4");
+  c4_audio.play();
+}
+function c_4(){
+  var c_4_audio=document.getElementById("c-4");
+  c_4_audio.play();
+}
+function d4(){
+  var d4_audio=document.getElementById("d4");
+  d4_audio.play();
+}
+function d_4(){
+  var d_4_audio=document.getElementById("d-4");
+  d_4_audio.play();
+}
+function e4(){
+  var e4_audio=document.getElementById("e4");
+  e4_audio.play();
+}
+function f4(){
+  var f4_audio=document.getElementById("f4");
+  f4_audio.play();
+}
+function f_4(){
+  var f_4_audio=document.getElementById("f-4");
+  f_4_audio.play();
+}
+function g4(){
+  var g4_audio=document.getElementById("g4");
+  g4_audio.play();
+}
+function g_4(){
+  var g_4_audio=document.getElementById("g-4");
+  g_4_audio.play();
+}
+function a4(){
+  var a4_audio=document.getElementById("a4");
+  a4_audio.play();
+}
+function a_4(){
+  var a_4_audio=document.getElementById("a-4");
+  a_4_audio.play();
+}
+function b4(){
+  var b4_audio=document.getElementById("b4");
+  b4_audio.play();
+}
